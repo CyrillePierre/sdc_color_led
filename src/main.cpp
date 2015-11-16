8017c927be35b8eeec93e82470b1dfd76865b329
Cyrille Pierre <cypierre58@gmail.com>
2015-11-16 23:43:52 +0100

    Application opérationnelle (changement de couleur).
    Actuellement, la couleur change toute seule et fait le tour complet des
    valeurs possibles de teintes en 10 secondes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <mbed.h>
+#include "color/converter.hpp"
 
 int main() {
     DigitalOut led(LED1);
+//    AnalogIn   pot(A0);
+    HSL<float> hsl;
+
+    hsl.s = 1;
+    hsl.l = .01;
 
     PwmOut red(D3);
     PwmOut green(D5);
     PwmOut blue(D6);
 
-    red = 1 - 1;
-    green = 1 - 1;
-    blue = 1 - 0;
+    red.period_ms(1);
+    green.period_ms(1);
+    blue.period_ms(1);
 
-    while(1){
-        led = !led;
-		wait(0.5);
+    for (;;) {
+        hsl.h += 0.001;
+        if (hsl.h > 1) hsl.h = 0;
+//        hsl.h = pot;
+
+        RGB<float> rgb = toRGB(hsl).getInvert();
+        red   = rgb.r;
+        green = rgb.g;
+        blue  = rgb.b;
+
+        wait_ms(10);
     }
 }
